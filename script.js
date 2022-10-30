6ad06e9e19e943461eab2781c1b8083c8c50c88d
Deepti Dubey <dubeydeepti1996@gmail.com>
2022-10-30 09:03:44 +0530

    corrected error

--- script.js
+++ script.js
@@ -20,15 +20,15 @@ const randomFunc = {
     symbol: getRandomSymbol
 };
 
-clipboardEl.addEventListener('click', () => {
-    const textarea = document.createElement('textarea');
-    const password = resultEl.innerText;
+// clipboardEl.addEventListener('click', () => {
+//     // const textarea = document.createElement('textarea');
+//     const password = resultEl.innerText;
 
-    if (!password)  { return; }
-    navigator.clipboard.writeText(password)
-});
+//     if (!password)  { return; }
+//     navigator.clipboard.writeText(password)
+// });
 
-generate.addEventListener('click', () => {
+generateEl.addEventListener('click', () => {
     const length= +lengthEl.value;
     const hasLower= lowercaseEl.checked;
     const hasUpper = uppercaseEl.checked;
@@ -38,31 +38,32 @@ generate.addEventListener('click', () => {
 
 });
 
-// //copy password
-// clipboardEl.addEventListener('click', ()=>{
-//     let Password = resultEl.innerHTML;
-//     if (Password === "") {
-//         console.log(Password, "hello");
-//         return
-//         alert("Please genrate Password first");  
-//     }
-//     navigator.clipboard.writeText(resultEl.innerHTML);
-//     alert('coppied the text:' +resultEl.innerHTML);
-// });
+//copy password
+clipboardEl.addEventListener('click', ()=>{
+    let Password = resultEl.innerHTML;
+    if (Password === "") {
+        console.log(Password, "hello");
+        return
+        alert("Please genrate Password first");  
+    }
+    navigator.clipboard.writeText(resultEl.innerHTML);
+    alert('coppied the text:' +resultEl.innerHTML);
+});
 
 function generatePassword(lower, upper, number, symbol, length) {
-    let generatePassword = '';
+    let generatePassword = "";
     const typesCount = lower + upper + number + symbol;
     
-    const typesArr= [{ lower}, {upper}, {number}, {symbol}].filter(
-        (item)=> Object.value(item)[0]);
+    const typesArr= [{ lower}, {upper}, {number}, {symbol}].filter
+    (
+        (item)=> Object.values(item)[0]);
 //selected type not included
         if (!typesCount === 0){
-            return '';
+            return "";
         }
 
         //loop
-        for (let i=0; i< length; i += typeCount){
+        for (let i=0; i< length; i += typesCount){
             typesArr.forEach((type) => {
                 const funcName = Object.keys(type)[0];
                 generatePassword += randomFunc[funcName]();
